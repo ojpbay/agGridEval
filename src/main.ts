c1ea4e62f7e85a5cbfd879713b8c06ed5575f660
Oliver Baylis <oli.baylis@gmail.com>
2018-04-07 08:51:17 +0100

    comment out enterprise

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {LicenseManager} from 'ag-grid-enterprise/main';
+//import {LicenseManager} from 'ag-grid-enterprise/main';
 
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
